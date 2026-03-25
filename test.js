c09443f7778fe0895e6abcbeae86d84bc1a7f470
Cyteon <cyteon@hackclub.app>
2026-03-25 10:29:40 +0100

    oops

--- test.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-const URI = "mongodb+srv://admin:i2OuWWnIAmpGD9Ow@cluster0.z34mo.mongodb.net/prod?appName=Cluster0";
-await mongoose.connect(URI);
-console.log("connected!");
-mongoose.disconnect();
